117fe924411052cf74cbeafa888362b681773adc
GeekUniversity <songwanquan@bonc.com.cn>
2021-08-01 18:00:58 +0800

    table define

---
+++ week-6/table.sql
@@ -0,0 +1,4 @@
+drop table ECommerce;
+create table ECommerce(user_id varchar(20), user_name varchar(20), goods_id bigint, order_id bigint);
+insert into ECommerce values('1001001', 'geek', 100, 110);
+select * from ECommerce;
